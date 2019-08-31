78442fa3c8e5a140f5a337fe1c88ce32d8f29f7e
Harry Zachariou <hcz1@student.le.ac.uk>
2019-08-31 20:39:28 +0100

    crc handler

--- handler.js
+++ handler.js
@@ -1,18 +1,14 @@
-'use strict';
+const qs = require('querystring');
+const crypto = require('crypto');
 
 module.exports.crc = async event => {
+
+  const hmac = crypto.createHmac('sha256', process.env.consumer_secret)
+    .update(event.queryStringParameters.crc_token).digest('base64');
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: JSON.stringify({
+      'response_token': 'sha256=' + hmac
+    }),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
